d47c74b304d6caf5c5629de2a969ef9c3017f943
ayalgimpel <ayalgimpel24@gmail.com>
2022-09-19 10:43:09 +0300

    ayal-update

---
+++ FakeLook-Server/src/core/db.js
@@ -0,0 +1,35 @@
+const sql = require('mssql');
+const config = require('config');
+const connectionString = config.get('db.connectionstring');
+const server = config.get('db.server');
+const password = config.get('db.password');
+const user = config.get('db.user');
+const database = config.get('db.database');
+
+let pool = undefined;
+
+const init = async () => {
+    const appPool = new sql.ConnectionPool({
+        server,
+        password,
+        user,
+        database
+    })
+
+    pool = await appPool.connect();
+    console.log('db connected..');
+    // appPool.connect().then(async function (pool) {
+    // _pool = pool;
+    //     console.log('db connected..');
+    // });
+}
+
+const getPool = () => {
+    return pool;
+}
+
+
+module.exports = {
+    init,
+    getPool
+}
--- FakeLook-Server/src/repositories/user.repository.js
+++ FakeLook-Server/src/repositories/user.repository.js
@@ -1,2 +1,13 @@
-const repository = {}
+const db = require('../core/db');
+
+const repository = {
+    async getAll() {
+        const pool = db.getPool();
+        console.log('repository getAll');
+        const result = await pool.query(`select * from [dbo].[User]`);
+        console.log('asdad');
+        return result.recordset
+    }
+}
+
 module.exports = repository;
