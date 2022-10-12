7cb3168ee5f42154a117b995677751b38f813dc0
DorSirton <Dor7479@gmail.com>
2022-10-12 11:42:23 +0300

    dor filter component

--- FakeLook-Client/src/app/Components/Home/Map/Map.component.ts
+++ FakeLook-Client/src/app/Components/Home/Map/Map.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, SimpleChanges} from '@angular/core';
+import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
 import { Loader } from '@googlemaps/js-api-loader';
 import Post from 'src/app/DataModels/Post';
 
@@ -13,64 +13,65 @@ export class MapComponent implements OnInit {
   @Input() displayPosts!: Post[]
   constructor(
   ) { }
-  async ngOnInit(){}
+  async ngOnInit() { }
 
-  async ngOnChanges(changes: SimpleChanges) { {
-    let loader = new Loader({
-      apiKey: "AIzaSyCiQwG8IXqXBbZWMfy13-Gdlb_8tLwe_hw"
-    });
-    
-    loader.load().then(() => {
-      navigator.geolocation.getCurrentPosition((location) => {
-        const centerLocation = {
-          lng: location.coords.longitude,
-          lat: location.coords.latitude
-        }
-       // console.log(centerLocation)
-        const displayMap = new google.maps.Map(<HTMLElement>document.getElementById('map'), {
-          center: centerLocation,
-          zoom: 16
-        })
-        let Mymarker = new google.maps.Marker({
-          position: centerLocation,
-          map: displayMap,
-          title:"My Location"
-        })
-        //console.log(this.displayPosts)
+  async ngOnChanges(changes: SimpleChanges) {
+    {
+      let loader = new Loader({
+        apiKey: "AIzaSyCiQwG8IXqXBbZWMfy13-Gdlb_8tLwe_hw"
+      });
+
+      loader.load().then(() => {
+        navigator.geolocation.getCurrentPosition((location) => {
+          const centerLocation = {
+            lng: location.coords.longitude,
+            lat: location.coords.latitude
+          }
+          // console.log(centerLocation)
+          const displayMap = new google.maps.Map(<HTMLElement>document.getElementById('map'), {
+            center: centerLocation,
+            zoom: 16
+          })
+          let Mymarker = new google.maps.Marker({
+            position: centerLocation,
+            map: displayMap,
+            title: "My Location"
+          })
+          //console.log(this.displayPosts)
           this.displayPosts.forEach(element => {
             let loc = {
-              lng:parseFloat(element.Longitude),
-              lat:parseFloat(element.Latitude)
+              lng: parseFloat(element.Longitude),
+              lat: parseFloat(element.Latitude)
             }
-          let image={
-            url:element.PhotoUrl,
-            size: new google.maps.Size(180,90),
+            let image = {
+              url: element.PhotoUrl,
+              size: new google.maps.Size(180, 90),
               origin: new google.maps.Point(0, 0),
               anchor: new google.maps.Point(17, 34),
               scaledSize: new google.maps.Size(100, 100)
-          }
+            }
 
             let marker = new google.maps.Marker({
               position: loc,
               map: displayMap,
-              title:element.Title,
-              optimized:false,
-              icon:image,
-              shape:{coords:[17,17,18],type:'circle'}
-              
+              title: element.Title,
+              optimized: false,
+              icon: image,
+              shape: { coords: [17, 17, 18], type: 'circle' }
+
             })
             var information = new google.maps.InfoWindow({
-              content: `<div><div class="userPostDetails"><h1>blbllb : </h1><img src=${element.PhotoUrl}></div><div class="post-detailes"><img src="post.image"><p>${element.Content}</p><p>Publish Date</p> <p>Number Of Likes ""</p><button>Like</button></div></div>`
-           });
-        
-           marker.addListener('click', function() {
+              content: `<div><div class="userPostDetails"><h1>blbllb : </h1><img src=${element.PhotoUrl}></div><div class="post-detailes"><p>${element.Content}</p><p>Publish Date</p> <p>Number Of Likes ""</p><button>Like</button></div></div>`
+            });
+
+            marker.addListener('click', function () {
               information.open(displayMap, marker);
-           });
+            });
           });
         })
       });
 
-    
+
 
 
     }
--- FakeLook-Server/src/middlewares/auth.middleware.js
+++ FakeLook-Server/src/middlewares/auth.middleware.js
@@ -1,7 +1,6 @@
 const jwt = require('jsonwebtoken');
 
 function authenticateToken(req, res, next) {
-    debugger;
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
     if (token === null) return res.sendStatus(401)
